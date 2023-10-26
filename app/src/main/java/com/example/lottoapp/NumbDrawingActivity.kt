12bfead01aad527ab69483ac7bd4b464abc9b8ef
Michał Dziak <276234@student.pwr.edu.pl>
2023-10-26 09:55:36 +0200

    Generator added

--- app/src/main/java/com/example/lottoapp/NumbDrawingActivity.kt
+++ app/src/main/java/com/example/lottoapp/NumbDrawingActivity.kt
@@ -1,9 +1,11 @@
 package com.example.lottoapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import kotlin.random.Random
+import kotlin.streams.toList
 
 class NumbDrawingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,13 +13,27 @@ class NumbDrawingActivity : AppCompatActivity() {
         setContentView(R.layout.activity_numb_drawing)
         val intent = intent
         val displayNbTextView = findViewById<TextView>(R.id.displayNbTextView)
-        val numbers: IntArray? = intent.getIntArrayExtra("SELECTEDNUMBERS")
-        var numbersString = ""
-        if (numbers != null) {
-            for (number in numbers){
-                numbersString += "$number "
+        var numbersString = intent.getIntArrayExtra("SELECTEDNUMBERS")?.joinToString(" ")
+        displayNbTextView.text = numbersString
+        val drawButton = findViewById<Button>(R.id.generateNbButton)
+        val displayGeneratedNb = findViewById<TextView>(R.id.displayNbTextView)
+        drawButton.setOnClickListener {
+            displayGeneratedNb.text = drawNumbers().joinToString(" ")
+        }
+
+    }
+
+    private fun drawNumbers(): IntArray{
+        val drawnNumbers: IntArray = IntArray(6)
+        var i = 0
+        while(i < 5){
+            var generatedNb = Random.nextInt(1, 49)
+            if (generatedNb !in drawnNumbers){
+                drawnNumbers[i] = generatedNb
+                i++
             }
         }
-        displayNbTextView.text = numbersString
+        return drawnNumbers
+
     }
 }
