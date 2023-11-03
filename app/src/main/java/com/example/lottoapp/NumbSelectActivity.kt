d1e6a237b10e1ca780dd4e9b7c43191c8d62e006
mwdziak <30974669+LethalDriver@users.noreply.github.com>
2023-11-03 15:15:14 +0100

    winnings calculation done

--- app/src/main/java/com/example/lottoapp/NumbSelectActivity.kt
+++ app/src/main/java/com/example/lottoapp/NumbSelectActivity.kt
@@ -8,44 +8,56 @@ import android.widget.NumberPicker
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 
-class NumbSelectActivity : AppCompatActivity() {
+class NumbSelectActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         val alertDialogBuilder = AlertDialog.Builder(this)
         alertDialogBuilder.setTitle("Number already selected")
         alertDialogBuilder.setMessage("You already selected this number, choose a different one.")
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.numb_layout)
         val intent = intent
+
         val name = intent.getStringExtra("NAME")
         val email = intent.getStringExtra("EMAIL")
         val phone = intent.getStringExtra("PHONE")
+
         val welcomeText = findViewById<TextView>(R.id.selectNumbersText)
         welcomeText.text = "$name, please select your Lucky numbers!"
+
         val numbersText = findViewById<TextView>(R.id.selectedNumbersView)
         val numbersPicker = findViewById<NumberPicker>(R.id.numbersPicker)
+
         numbersPicker.maxValue = 49
         numbersPicker.minValue = 1
+
         val selectButton = findViewById<Button>(R.id.selectButton)
+
         val getRichButton = findViewById<Button>(R.id.getRichButton)
         getRichButton.isEnabled = false
+
         val numbersArray = IntArray(6)
         var i = 0
         var text = ""
+
         selectButton.setOnClickListener {
             var selectedNumber = numbersPicker.value
             if (selectedNumber !in numbersArray){
+
                 numbersArray[i++] = selectedNumber
                 text += selectedNumber.toString()
                 text = "$text "
                 numbersText.text = text
-            } else {
-                val alertDialog = alertDialogBuilder.create()
-                alertDialog.show()
-            }
-            if (i > numbersArray.size - 1) {
-                selectButton.isEnabled = false
-                getRichButton.isEnabled = true
-            }
+                if (i > numbersArray.size - 1) {
+                    selectButton.isEnabled = false
+                    getRichButton.isEnabled = true
+                }
+                showErrorSnackBar(
+                    resources.getString(R.string.numbSelectedSuccessful),
+                    errorMessage = false
+                )
+            } else showErrorSnackBar(resources.getString(R.string.numbSelectedError), true)
+
         }
         getRichButton.setOnClickListener{
             val intent = Intent(this, NumbDrawingActivity::class.java)
