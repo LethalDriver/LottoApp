221af7af909c3fbba2ce67ea45379d9067718e73
matlic <268389@student.pwr.edu.pl>
2023-11-10 14:29:18 +0100

    Labwork during classes

--- app/src/main/java/com/example/lottoapp/MainActivity.kt
+++ app/src/main/java/com/example/lottoapp/MainActivity.kt
@@ -11,41 +11,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val switchView = findViewById<Switch>(R.id.ageSwitch)
-        val buttonGame = findViewById<Button>(R.id.gameButton)
-        buttonGame.isEnabled = false
+        val loginButton = findViewById<Button>(R.id.loginButton)
 
-        switchView.setOnCheckedChangeListener { _, isChecked ->
-            buttonGame.isEnabled = isChecked
-            if (isChecked){
-                switchView.text = "YES"
-            }
-            else{
-                switchView.text = "NO"
-            }
+        val inputEmail = findViewById<EditText>(R.id.editTextEmail)
+        val inputPassword = findViewById<EditText>(R.id.editTextPassword)
+        loginButton.setOnClickListener {
 
-        }
-        val inputName = findViewById<EditText>(R.id.editTextName)
-        val inputMail = findViewById<EditText>(R.id.editTextEmailAddress)
-        val inputPhone = findViewById<EditText>(R.id.editTextPhone)
-        buttonGame.setOnClickListener {
-
-            var name = inputName.text.toString()
-            if (name.isEmpty()) {
-                name = "unknown"
-            }
-            var email = inputMail.text.toString()
+            var email = inputEmail.text.toString()
             if (email.isEmpty()) {
                 email = "unknown"
             }
-            var phone = inputPhone.text.toString()
-            if (phone.isEmpty()) {
-                phone = "unknown"
+            var password = inputPassword.text.toString()
+            if (password.isEmpty()) {
+                password = "unknown"
             }
             val intent = Intent(this, NumbSelectActivity::class.java)
-            intent.putExtra("NAME", name)
             intent.putExtra("EMAIL", email)
-            intent.putExtra("PHONE", phone)
+            intent.putExtra("PASSWORD", password)
             startActivity (intent)
             }
 
